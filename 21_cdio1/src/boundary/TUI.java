8612cce4f9b5c795e1d630f239c96f7dd7cc7baa
Dannie M. Fjordside <Dannie_Fjordside@hotmail.com>
2016-02-23 07:30:16 +0100

    Added comments to TUI

--- 21_cdio1/src/boundary/TUI.java
+++ 21_cdio1/src/boundary/TUI.java
@@ -6,14 +6,16 @@ public class TUI implements IUI {
 
 	private Scanner scanner;
 
+	// get the input from user when user press enter
 	@Override
 	public String getInput() {
 		scanner = new Scanner(System.in);
-		String catched = scanner.nextLine()/*.trim()*/;
+		String catched = scanner.nextLine();
 		
 		return catched;
 	}
 
+	// print the info given by the controller
 	@Override
 	public void showInput(String msg) {
 		System.out.println(msg);
