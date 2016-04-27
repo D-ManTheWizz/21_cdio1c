3fe18c0d5f4c528151cf59ba08ec3739d2538caa
abdishakuur <abdishakuur@10.16.163.61>
2016-04-27 10:54:54 +0200

    test

--- 21_cdio1/src/controller/Controller.java
+++ 21_cdio1/src/controller/Controller.java
@@ -15,6 +15,7 @@ public class Controller {
 	private IOperatorDTO oDTO;
 	private IUI UI;
 	private ControllerText controllerText;
+	private boolean trancient=true;
 	
 	private int fixedAdmin = 10;
 	
@@ -30,12 +31,15 @@ public class Controller {
 	}
 
 	public void run() throws DALException {
-		
+		if(trancient){
 		oDAO.setOperator(10, "Admin Jensen", "AdJe", "123456-1234", "1234Pass");
 		oDAO.setOperator(11, "Operator Jensen", "OpJe", "234567-2345", "2345Pass");
 		oDAO.setOperator(12, "Svend Nielsen", "SvNi", "345678-3456", "3456Pass");
 		oDAO.setOperator(13, "Hans Christensen", "HaCh", "456789-4567", "4567Pass");
-		
+		}
+		if(!trancient){
+			
+		}
 		startMenu();
 		
 	}
